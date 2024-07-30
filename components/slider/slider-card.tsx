afd7bdbc8f3a4131fc219c198b979cd9bce9bbb2
SamiFraca <fracasami@gmail.com>
2024-07-30 14:01:32 +0200

    fix mobile slider card width

--- components/slider/slider-card.tsx
+++ components/slider/slider-card.tsx
@@ -12,7 +12,7 @@ export const SliderCard: React.FC<SliderCardProps> = ({
   description,
 }) => {
   return (
-    <div className="border-2 border-gray-800 p-4 rounded-md w-[450px] flex-shrink-0">
+    <div className="border-2 border-gray-800 p-4 rounded-md md:w-[28.125rem] w-[20rem] flex-shrink-0">
       <div className="flex gap-4 items-center mb-4">
         <h3 className="text-xl">{text}</h3> {icon}
       </div>

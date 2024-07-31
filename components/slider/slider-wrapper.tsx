3055f55772047d09312e771d8f58f3a111a79083
SamiFraca <fracasami@gmail.com>
2024-07-31 14:39:56 +0200

    Use arbitrary values to hide scrollbar

--- components/slider/slider-wrapper.tsx
+++ components/slider/slider-wrapper.tsx
@@ -4,7 +4,7 @@ type sliderWrapperProps = {
 export const SliderWrapper: React.FC<sliderWrapperProps> = ({ children }) => {
   return (
     <div className="relative  overflow-hidden mt-12 roboto">
-      <ul className="flex flex-nowrap gap-4 overflow-auto no-scrollbar">{children}</ul>
+      <ul className="flex flex-nowrap gap-4 overflow-auto  [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">{children}</ul>
       <div className="gradient-overlay overflow-auto"></div>
     </div>
   );

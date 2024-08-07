eaa22d0287b6100cfe884a84b5b8834beae18912
SamiFraca <fracasami@gmail.com>
2024-08-07 09:18:42 +0200

    Fix multiple cookies handling when logging out

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,31 +1,47 @@
 // app/api/auth/signout/route.ts
 
-import { NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabaseClient'; // Import your Supabase client
-import { serialize } from 'cookie';
-import { cookies } from 'next/headers';
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/supabaseClient"; // Import your Supabase client
+import { serialize } from "cookie";
+import { cookies } from "next/headers";
 
 export async function POST() {
   // Sign out from Supabase
   const { error } = await supabase.auth.signOut();
-  const cookieStore = cookies()
 
   if (error) {
-    return NextResponse.json({ error: 'Failed to sign out' }, { status: 500 });
+    return NextResponse.json({ error: "Failed to sign out" }, { status: 500 });
   }
+  const allCookies = cookies();
+  const authTokenCookieArray: Array<{ name: string; value: string }> = [];
 
-  // Set the cookie to expire
-  const cookieHeader = serialize('look for "auth-token" cookie name', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    path: '/',
-    maxAge: -1, // Expire the cookie
+  allCookies.getAll().map((cookie) => {
+    if (cookie.name.match("auth-token")) {
+      authTokenCookieArray.push(cookie);
+    }
   });
 
-  return NextResponse.json({ message: 'Signed out successfully' }, {
-    headers: {
-      'Set-Cookie': cookieHeader,
-    },
-  });
+  if (authTokenCookieArray.length > 0) {
+    const cookieHeaders = authTokenCookieArray.map((cookie) => {
+      return serialize(cookie.name, "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
+        maxAge: -1, // Expire the cookie
+      });
+    });
+
+    const cookiesString = cookieHeaders.join(", ");
+
+    return NextResponse.json(
+      { message: "Signed out successfully" },
+      {
+        headers: {
+          "Set-Cookie": cookiesString,
+        },
+      }
+    );
+  }
+  return NextResponse.json({ message: "No cookies to sign out" });
 }

20c1a25b6abf5cbc49ef4d3a08bf9aa44f9219b0
SamiFraca <fracasami@gmail.com>
2024-08-12 15:09:15 +0200

    Add edit img button on sidenav

--- components/profile/side-nav.tsx
+++ components/profile/side-nav.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import defaultImageAvatar from "@/assets/img/default-profile.png";
 import getUser from "@/lib/getUser";
-import { MapPinIcon } from "lucide-react";
+import { MapPinIcon, Pencil } from "lucide-react";
 import Link from "next/link";
 export async function SideNav() {
   const { user } = await getUser();
   return (
     <aside className="p-4 pt-0 flex flex-col gap-2 w-64">
-      <Image
-        src={defaultImageAvatar.src}
-        width={120}
-        height={120}
-        alt="profile"
-        className="mx-auto"
-      />
+      <div className="flex w-full justify-center items-center ">
+        <div className="w-max relative">
+          <Image
+            src={defaultImageAvatar.src}
+            width={120}
+            height={120}
+            alt="profile"
+            className="mx-auto"
+          />
+          <div className="absolute bottom-0 right-0 rounded-full bg-gray-800 bg-opacity-80 p-2 hover:bg-opacity-100 cursor-pointer">          <Pencil width={20} height={20} className="text-white"  /></div>
+        </div>
+      </div>
 
       <p className="font-semibold text-xl capitalize">
         {user?.user_metadata.lastName && (
@@ -29,8 +34,12 @@ export async function SideNav() {
 
       <nav>
         <ul className="flex flex-col gap-2">
-          <li><Link href={'/profile'}>Profile</Link></li>
-          <li><Link href={'/profile/projects'}>Your projects</Link></li>
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+          <li>
+            <Link href={"/profile/projects"}>Your projects</Link>
+          </li>
         </ul>
       </nav>
     </aside>

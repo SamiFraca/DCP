7beda2bd158a6817a916f7bf5a4af106f59159b4
SamiFraca <fracasami@gmail.com>
2024-09-18 13:39:44 +0200

    Add text area to profile page and ref

--- app/[locale]/profile/page.tsx
+++ app/[locale]/profile/page.tsx
@@ -3,14 +3,30 @@
 import { Divider } from "@/components/divider";
 import CustomPinProjectsList from "@/components/profile/custom-pin-projects-list";
 import { CustomPinProjectsModal } from "@/components/profile/custom-pin-projects-modal";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { useUser } from "@/context/user-context";
-import { Pin } from "lucide-react";
+import { Pencil, Pin } from "lucide-react";
+import { useRef, useState } from "react";
 
 export default function Profile() {
   const { user, isLoading } = useUser();
+  const [isShowAreaDescriptionEnabled, setIsShowAreaDescriptionEnabled] =
+    useState<boolean>(false);
+    const descriptionTextAreaRef = useRef<HTMLTextAreaElement>(null);
+
 
   if (isLoading) {
     return <div>Loading..</div>;
+  }
+
+  const handleModifyUserDescription = () => {
+    setIsShowAreaDescriptionEnabled(!isShowAreaDescriptionEnabled);
+  };
+  const sendNewUserDescriptionData = () => {
+    console.log(descriptionTextAreaRef.current?.value)
+    
+    // user?.user_metadata.description =
   }
 
   return (
@@ -23,8 +39,35 @@ export default function Profile() {
       </p>
       <Divider className="my-4" />
       <div className="mb-8">
-        <p className="text-xl mb-4">About yourself</p>
-        <p>{user?.user_metadata.description ?? 'test'}</p>
+        <div className="flex gap-4 items-center mb-4">
+          <p className="text-xl">About yourself</p>
+          <Button
+            variant={"ghost"}
+            className="dark:text-gray-400 p-2"
+            onClick={() => {
+              handleModifyUserDescription();
+            }}
+          >
+            <Pencil width={20} height={20} />
+          </Button>
+        </div>
+        {isShowAreaDescriptionEnabled ? (
+          <>
+            <Textarea placeholder="Type your new description here"  ref={descriptionTextAreaRef} />
+            <div className="flex gap-4">
+              <Button className="mt-4" variant="destructive" onClick={() => setIsShowAreaDescriptionEnabled(false)}>
+                Cancel
+              </Button>
+              <Button className="mt-4" variant="default" onClick={() => {
+                sendNewUserDescriptionData()
+              }}>
+                Save
+              </Button>
+            </div>
+          </>
+        ) : (
+          <p>{user?.user_metadata?.description || "No description provided"}</p>
+        )}
       </div>
       <div className="flex">
         <div className="flex flex-col grow">

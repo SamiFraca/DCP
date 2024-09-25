bcb8b692ee1f1d9aad6cbb614e062610e6e33ab6
SamiFraca <fracasami@gmail.com>
2024-09-25 09:23:11 +0200

    Remove unused comments

--- __tests__/e2e/register.spec.ts
+++ __tests__/e2e/register.spec.ts
@@ -12,18 +12,18 @@ test("User registration", async ({ page }) => {
   await page.fill('input[name="options.data.country"]', "testCountry");
   await page.selectOption('select[name="options.data.mainField"]', 'Sports');
 
-   // Listen for console messages (optional)
+
    page.on('console', msg => console.log(msg.text()));
 
    // Submit the registration form
    const [response] = await Promise.all([
-     page.waitForResponse(response => response.url().includes('/register') && response.status() === 200), // Adjust this to the actual registration URL
+     page.waitForResponse(response => response.url().includes('/register') && response.status() === 200), 
      page.click('button[type="submit"]'),
    ]);
 
    // Check if the response was successful
    expect(response.ok()).toBeTruthy();
-  // Wait for the URL to change to the expected one
+
   await page.goto(`/en?register=success`);
 
   // Assert that the URL is as expected
